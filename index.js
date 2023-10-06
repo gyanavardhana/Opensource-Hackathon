69bc7c7b5989abb39c46acb1af8e3232f54df2e9
sigireddy viswesh <98519767+Viswesh934@users.noreply.github.com>
2023-10-06 22:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,82 @@ addbut.addEventListener('click', ()=>{
    const myt = document.getElementById('mylist');
    myt.appendChild(newone);
 });
+/*Pomodoro*/
+const timerDisplay = document.getElementById("timer");
+
+function PomodorTimer(worktime, breaktime) {
+    this.worktime = worktime * 60;
+    this.breaktime = breaktime * 60;
+    this.timer = 0;
+    this.interval = null;
+}
+
+PomodorTimer.prototype.startWork = function () {
+    if (this.timer === 0) {
+        this.timer = this.worktime;
+    }
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+        this.timer--;
+        if (this.timer === 0) {
+            clearInterval(this.interval);
+            this.startBreak();
+        }
+        updateTimerDisplay();
+    }, 1000);
+};
+
+PomodorTimer.prototype.startBreak = function () {
+    if (this.timer === 0) {
+        this.timer = this.breaktime;
+    }
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+        this.timer--;
+        if (this.timer === 0) {
+            clearInterval(this.interval);
+            this.startWork();
+        }
+        updateTimerDisplay();
+    }, 1000);
+};
+
+PomodorTimer.prototype.stop = function () {
+    clearInterval(this.interval);
+};
+
+PomodorTimer.prototype.reset = function () {
+    clearInterval(this.interval);
+    this.timer = 0;
+    updateTimerDisplay();
+};
+
+const timer = new PomodorTimer(25, 5);
+
+function updateTimerDisplay() {
+    const minutes = Math.floor(timer.timer / 60);
+    const seconds = timer.timer % 60;
+    timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+const start = document.createElement("button");
+start.textContent = "Start";
+start.addEventListener("click", () => {
+    timer.startWork();
+});
+
+const stop = document.createElement("button");
+stop.textContent = "Stop";
+stop.addEventListener("click", () => {
+    timer.stop();
+});
+
+const reset = document.createElement("button");
+reset.textContent = "Reset";
+reset.addEventListener("click", () => {
+    timer.reset();
+});
+
+document.body.appendChild(start);
+document.body.appendChild(stop);
+document.body.appendChild(reset);
